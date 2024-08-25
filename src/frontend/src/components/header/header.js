ada5d1e90f4ed6cb17e57a728038003b18aa843e
Ethann Schneider <ethann@shkermit.ch>
2024-08-25 22:38:36 +0200

    Added menu

--- src/frontend/src/components/header/header.js
+++ src/frontend/src/components/header/header.js
@@ -2,8 +2,35 @@ import './header.css';
 
 function App() {
   return (
-    <header className="Header">
-      <h1>The Golden Pearl</h1>
+    <header className="Header" class="sticky-top">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/" >
+            <img src="logo256.png" alt="logo" width="70" />
+          </a>
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="/">Who are we ?</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/daily_menu">Daily Menu</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/">Menu</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/">History</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 }
--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -11,6 +11,9 @@ import {
   BrowserRouter,
   Routes
 } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
 
 export default function App() {
   return (
